b2b762ddbc14b1631b61b6c9c52ff7574a943230
Hugo <hugo.duchemann@hotmail.com>
2019-04-30 13:36:39 +0200

    Update register.php

--- register.php
+++ register.php
@@ -4,13 +4,15 @@ $database="eceamazon";
 
 $db_handle = mysqli_connect('localhost','root','');
 $db_found = mysqli_select_db($db_handle,$database);
+
+
 $nom=isset($_POST["nom"])?$_POST["nom"]:"";
 $prenom=isset($_POST["prenom"])?$_POST["prenom"]:"";
 $mail=isset($_POST["mail"])?$_POST["mail"]:"";
 $pseudo=isset($_POST["pseudo"])?$_POST["pseudo"]:"";
 $mdp=isset($_POST["mdp"])?$_POST["mdp"]:"";
 $adresse=isset($_POST["adresse"])?$_POST["adresse"]:"";
-$statut=isset($_POST['statut'])?$_POST['statut']:'';
+
 
 
 $error="";
@@ -21,18 +23,15 @@ if($pseudo==""){$error.="Pseudo vide <br>";}
 if($mdp==""){$error.="Mot de Passe vide <br>";}
 if($adresse==""){$error.="Adresse vide <br>";}
 if ($db_found) {
-	if ($statut=='acheteur'){
+	if ($error==""){
 		$sql="INSERT INTO acheteurs VALUES ('$nom','$prenom','$mail','$pseudo','$mdp','$adresse') ";
 		$result = mysqli_query($db_handle, $sql);
-		echo "Personne ajouté dans la table acheteurs";
+		echo "Votre compte a été créé avec succès";
 	}
-	if ($statut=='vendeur'){
-		$sql="INSERT INTO vendeurs VALUES ('$nom','$prenom','$mail','$pseudo','$mdp','$adresse') ";
-		$result = mysqli_query($db_handle, $sql);
-		echo "Personne ajouté dans la table vendeurs";
+	else {
+		echo "Erreur : $error";
 	}
-	
-	
+		
 }
 else{
 	echo "database not found";
